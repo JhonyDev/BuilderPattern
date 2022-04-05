56fb9a85f732abe98d4fed7f279a30abd00c4733
jhonydev <star.jk901@gmail.com>
2022-04-05 12:43:08 +0500

    Initial commit

---
+++ src/com/assignment/homework/products/Kitten.java
@@ -0,0 +1,22 @@
+package com.assignment.homework.products;
+
+public class Kitten extends Animal {
+    public Kitten(String Arm, String body, String head, String Leg, String Tail) {
+        super(Arm, body, head, Leg, Tail);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(
+                "Kitten Eating"
+        );
+    }
+
+    @Override
+    public void showMe() {
+        System.out.println(
+                getArm() + " \n" + getBody() + " \n" + getHead() + " \n" + getBody() + " \n" + getLeg() + " \n" + getTail()
+        );
+    }
+}
+
---
+++ src/com/assignment/homework/products/Monkey.java
@@ -0,0 +1,21 @@
+package com.assignment.homework.products;
+
+public class Monkey extends Animal {
+    public Monkey(String Arm, String body, String head, String Leg, String Tail) {
+        super(Arm, body, head, Leg, Tail);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(
+                "Monkey Eating"
+        );
+    }
+
+    @Override
+    public void showMe() {
+        System.out.println(
+                getArm() + " \n" + getBody() + " \n" + getHead() + " \n" + getBody() + " \n" + getLeg() + " \n" + getTail()
+        );
+    }
+}
